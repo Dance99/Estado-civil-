c998557e9417da584ecfa96926ee8a765baa5047
Daniela <nam991019@gmail.com>
2024-11-03 20:20:15 -0500

    estado civil

---
+++ script.js
@@ -0,0 +1,23 @@
+let nombre =prompt("Ingresa tu nombre")
+
+let Estado =prompt("Ingresa tu estado civil (1: Soltero, 2: Casado, 3: Separado, 4: Viudo, 5: Unión libre):" );
+
+switch ("Estado civil") {
+    case "1":
+        estadocivil ="Soltero";
+        break;
+    case "1":
+        estadocivil ="Casado";
+         break;
+    case "1":
+        estadocivil ="Separado";
+        break;
+    case "1":
+        estadocivil ="Viudo";
+        break;
+    case "1":
+        estadocivil ="Union libre";
+        break;
+    
+}
+ 
