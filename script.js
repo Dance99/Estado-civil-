b20bf98621a59db4434eafa6fdbd0c52c663024f
Daniela <nam991019@gmail.com>
2024-11-03 20:55:12 -0500

    estado civil resultado

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ let nombre =prompt("Ingresa tu nombre")
 
 let Estado =prompt("Ingresa tu estado civil (1: Soltero, 2: Casado, 3: Separado, 4: Viudo, 5: Unión libre):" );
 
-switch ("Estado civil") {
+switch ("Estado") {
     case "1":
-        estadocivil ="Soltero";
+        mensajestadocivil ="Soltero";
         break;
-    case "1":
-        estadocivil ="Casado";
+    case "2":
+        mensajestadocivil ="Casado";
          break;
-    case "1":
-        estadocivil ="Separado";
+    case "3":
+        mensajestadocivil ="Separado";
         break;
-    case "1":
-        estadocivil ="Viudo";
+    case "4":
+        mensajestadocivil ="Viudo";
         break;
-    case "1":
-        estadocivil ="Union libre";
+    case "5":
+        mensajestadocivil ="Union libre";
         break;
-    
+    default: 
+         mensajestadocivil = "Estado civil no reconocido.";
 }
  
+alert(`Hola ${nombre}, tu estado civil es: ${Estado}`);
